80a54eb1756c768f489f4303a8c4b2558b446214
rnburn <ryan.burn@gmail.com>
2017-04-07 10:37:24 -0400

    Fix for_each_index grainularity.

--- include/satyr/for_each_index.h
+++ include/satyr/for_each_index.h
@@ -19,27 +19,40 @@ void for_each_index(Policy policy, std::array<index_t, K> extents,
   }
 }
 
-template <Policy Policy, size_t K, class Functor>
-  requires has_policy_v<grainularity, Policy>
-void for_each_index(Policy policy, std::array<index_t, K> extents,
-    Functor f) {
-  auto grainularity = get_policy<satyr::grainularity>(policy);
-  if (grainularity.value == 1)
-    return for_each_index(
-        policy | serial_v, extents, f
-    );
-  if constexpr(K == 1) { for_(policy, 0, extents[0], f); }
-  else {
-    auto n = extents[K - 1];
+namespace detail {
+template <class Policy, size_t K, class Functor>
+void parallel_for_each_index_impl(Policy policy,
+                                  std::array<index_t, K-1> cardinalities,
+                                  std::array<index_t, K> extents,
+                                  Functor f) {
+  if constexpr(K == 1) { 
+    for_(policy, 0, extents[0], f); 
+  } else {
+    auto n = cardinalities[K - 2];
+    auto grainularity = get_policy<satyr::grainularity>(policy);
     auto grainularity_new =
         satyr::grainularity{std::max<index_t>(grainularity.value / n, 1)};
-    for_(grainularity, 0, n, [=](index_t i) {
+    for_(grainularity_new, 0, n, [=](index_t i) {
       auto f_prime = [=](auto... indexes) { f(indexes..., i); };
-      return for_each_index(
-          policy | grainularity_new,
+      return parallel_for_each_index_impl(
+          policy,
+          reinterpret_cast<const std::array<index_t, K - 2>&>(extents),
           reinterpret_cast<const std::array<index_t, K - 1>&>(extents),
           f_prime);
     });
   }
+}
+}
+
+template <Policy Policy, size_t K, class Functor>
+  requires has_policy_v<grainularity, Policy>
+void for_each_index(Policy policy, std::array<index_t, K> extents, Functor f) {
+  std::array<index_t, K - 1> cardinalities;
+  index_t cardinality = 1;
+  for (index_t i = 0; i < K - 1; ++i) {
+    cardinality *= extents[i];
+    cardinalities[i] = cardinality;
+  }
+  detail::parallel_for_each_index_impl(policy, cardinalities, extents, f);
 }
 } // namespace satyr
--- test/test_for.cpp
+++ test/test_for.cpp
@@ -1,4 +1,16 @@
 #include <satyr/for_each_index.h>
+#include <cassert>
+using namespace satyr;
+
+void test_for_each_index() {
+  const index_t m = 10, n = 20;
+  index_t a[m][n] = {};
+  for_each_index(grainularity{5}, std::array<index_t, 2>{m, n},
+                 [&](index_t i, index_t j) { a[i][j] = i + m * j; });
+  for (index_t i=0; i<m; ++i)
+    for (index_t j=0; j<n; ++j)
+      assert(a[i][j] == i+m*j);
+}
 
 int main() {
   return 0;
