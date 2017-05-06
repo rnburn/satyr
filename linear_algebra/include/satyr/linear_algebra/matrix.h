43e3b22765ede2c669b65d80ac2a92ba1a36d8f5
Ryan Burn <ryan.burn@gmail.com>
2017-05-06 17:19:56 -0400

    Adding matrix types.

--- linear_algebra/include/satyr/linear_algebra/matrix.h
+++ linear_algebra/include/satyr/linear_algebra/matrix.h
@@ -7,17 +7,77 @@ namespace satyr {
 // matrix
 //------------------------------------------------------------------------------
 template <Scalar Scalar>
-using matrix = n_array<Scalar, 2, structure_general>;
+using matrix = n_array<Scalar, 2, general_structure>;
 
 //------------------------------------------------------------------------------
 // matrix_view
 //------------------------------------------------------------------------------
 template <Scalar Scalar>
-using matrix_view = n_array_view<Scalar, 2, structure_general>;
+using matrix_view = n_array_view<Scalar, 2, general_structure>;
 
 //------------------------------------------------------------------------------
 // matrix_cview
 //------------------------------------------------------------------------------
 template <Scalar Scalar>
-using matrix_cview = n_array_cview<Scalar, 2, structure_general>;
+using matrix_cview = n_array_cview<Scalar, 2, general_structure>;
+
+//------------------------------------------------------------------------------
+// upper_triangular_matrix
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using upper_triangular_matrix = n_array<Scalar, 2, upper_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// upper_triangular_matrix_view
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using upper_triangular_matrix_view =
+    n_array_view<Scalar, 2, upper_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// upper_triangular_matrix_cview
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using upper_triangular_matrix_cview =
+    n_array_cview<Scalar, 2, upper_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// lower_triangular_matrix
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using lower_triangular_matrix = n_array<Scalar, 2, lower_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// lower_triangular_matrix_view
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using lower_triangular_matrix_view =
+    n_array_view<Scalar, 2, lower_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// lower_triangular_matrix_cview
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using lower_triangular_matrix_cview =
+    n_array_cview<Scalar, 2, lower_triangular_structure>;
+
+//------------------------------------------------------------------------------
+// symmetric_matrix
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using symmetric_matrix = n_array<Scalar, 2, symmetric_structure>;
+
+//------------------------------------------------------------------------------
+// symmetric_matrix_view
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using symmetric_matrix_view =
+    n_array_view<Scalar, 2, symmetric_structure>;
+
+//------------------------------------------------------------------------------
+// symmetric_matrix_cview
+//------------------------------------------------------------------------------
+template <Scalar Scalar>
+using symmetric_matrix_cview =
+    n_array_cview<Scalar, 2, symmetric_structure>;
 } // namespace satyr
--- linear_algebra/include/satyr/linear_algebra/structure.h
+++ linear_algebra/include/satyr/linear_algebra/structure.h
@@ -1,6 +1,35 @@
 #pragma once
 
 #include <satyr/n_array.h>
+#include <satyr/uplo.h>
 
 namespace satyr {
+//------------------------------------------------------------------------------
+// triangular_structure
+//------------------------------------------------------------------------------
+template <uplo_t Uplo>
+struct triangular_structure : refines_structure<general_structure> {
+  static constexpr bool is_equal_dimensional = true;
+  static constexpr uplo_t uplo = Uplo;
+};
+
+using upper_triangular_structure = triangular_structure<uplo_t::upper>;
+using lower_triangular_structure = triangular_structure<uplo_t::lower>;
+
+//------------------------------------------------------------------------------
+// symmetric_structure
+//------------------------------------------------------------------------------
+struct symmetric_structure : refines_structure<general_structure> {
+  static constexpr bool is_equal_dimensional = true;
+  static constexpr uplo_t uplo = uplo_t::lower;
+};
+
+//------------------------------------------------------------------------------
+// diagonal_structure
+//------------------------------------------------------------------------------
+struct diagonal_structure : refines_structure<general_structure>,
+                            refines_structure<upper_triangular_structure>,
+                            refines_structure<lower_triangular_structure>,
+                            refines_structure<symmetric_structure> {
+};
 } // namespace satyr
