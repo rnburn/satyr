9b4f57be41293636cd8164c717c9c245887a5f4a
Ryan Burn <ryan.burn@gmail.com>
2017-05-28 00:05:45 -0400

    Link in mkl libraries.

--- cmake/Modules/FindMKL.cmake
+++ cmake/Modules/FindMKL.cmake
@@ -9,6 +9,12 @@ endif()
 find_path(MKL_INCLUDE_DIR mkl.h PATHS ${MKL_ROOT}/include)
 
 if (NOT MKL_LIBRARIES)
-  find_library(MKL_LIBRARY mkl_intel PATHS ${MKL_ROOT}/lib)
-  set(MKL_LIBRARIES ${MKL_LIBRARY})
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_ILP64 -m64")
+  set(CMAKE_SHARED_LINKER_FLAGS 
+    "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-rpath,${MKL_ROOT}/lib")
+  find_library(MKL_LIBRARY mkl_intel_ilp64 PATHS ${MKL_ROOT}/lib)
+  find_library(MKL_SEQUENTIAL_LIBRARY mkl_sequential PATHS ${MKL_ROOT}/lib)
+  find_library(MKL_CORE_LIBRARY mkl_core PATHS ${MKL_ROOT}/lib)
+  set(MKL_LIBRARIES
+    "${MKL_LIBRARY} ${MKL_SEQUENTIAL_LIBRARY} ${MKL_CORE_LIBRARY}")
 endif()
