618aa4f6627c8a0f2ab6fd3f64d1daf984fae8e1
Ryan Burn <ryan.burn@gmail.com>
2017-07-23 17:45:00 -0400

    Add parallelized triangular iteration.

--- core/include/satyr/for_each_index.h
+++ core/include/satyr/for_each_index.h
@@ -10,20 +10,20 @@ namespace satyr {
 //------------------------------------------------------------------------------
 // for_each_index_triangular
 //------------------------------------------------------------------------------
-template <uplo_t Uplo, Policy Policy, IndexFunctor<2> Functor>
-  requires has_policy_v<grainsize, Policy>
-void for_each_index_triangular(Policy policy, index_t n, Functor f) {
-  auto grainsize_outer = subdivide(get_policy<grainsize>(policy), n + 1);
-  auto n_div_2 = n / 2;
-  auto p = n / 2 + (n % 2);
-  for_(grainsize_outer, 0, p, [=](index_t j1) {
-    auto j2 = n - j1 - 1;
-    if (j1 != j2) {
-      detail::for_each_index_triangular_impl<Uplo>(policy, j1, n, f);
-      detail::for_each_index_triangular_impl<Uplo>(policy, j2, n, f);
-    } else {
-      detail::for_each_index_triangular_impl<Uplo>(policy, j1, n, f);
-    }
-  });
-}
+/* template <uplo_t Uplo, Policy Policy, IndexFunctor<2> Functor> */
+/*   requires has_policy_v<grainsize, Policy> */
+/* void for_each_index_triangular(Policy policy, index_t n, Functor f) { */
+/*   auto grainsize_outer = subdivide(get_policy<grainsize>(policy), n + 1); */
+/*   auto n_div_2 = n / 2; */
+/*   auto p = n / 2 + (n % 2); */
+/*   for_(grainsize_outer, 0, p, [=](index_t j1) { */
+/*     auto j2 = n - j1 - 1; */
+/*     if (j1 != j2) { */
+/*       detail::for_each_index_triangular_impl<Uplo>(policy, j1, n, f); */
+/*       detail::for_each_index_triangular_impl<Uplo>(policy, j2, n, f); */
+/*     } else { */
+/*       detail::for_each_index_triangular_impl<Uplo>(policy, j1, n, f); */
+/*     } */
+/*   }); */
+/* } */
 } // namespace satyr
--- core/include/satyr/tbb_for.h
+++ core/include/satyr/tbb_for.h
@@ -28,19 +28,19 @@ void for_(Policy policy, index_t first, index_t last, F f) {
 // for_each_index
 //------------------------------------------------------------------------------
 namespace detail {
-template <size_t K>
-struct tbb_k_blocked_range {
-  explicit tbb_k_blocked_range(const k_blocked_range<K>& range_)
+template <class Range>
+struct tbb_blocked_range {
+  explicit tbb_blocked_range(const Range& range_)
       : range{range_} {}
 
-  tbb_k_blocked_range(
-      tbb_k_blocked_range& other, 
+  tbb_blocked_range(
+      tbb_blocked_range& other, 
       tbb::split /*split*/)
     : range{other.range, split_range_v} {}
 
   static const bool is_splittable_in_proportion = true;
 
-  tbb_k_blocked_range(tbb_k_blocked_range& other,
+  tbb_blocked_range(tbb_blocked_range& other,
                       const tbb::proportional_split& split)
       : range{other.range,
               proportional_split_range{split.left(), split.right()}} {}
@@ -49,7 +49,7 @@ struct tbb_k_blocked_range {
 
   bool is_divisible() const { return range.is_divisible(); }
 
-  k_blocked_range<K> range;
+  Range range;
 };
 } // namespace detail
 
@@ -58,40 +58,35 @@ template <Policy Policy, size_t K, IndexFunctor<K> Functor>
 void for_each_index(Policy policy, std::array<index_t, K> extents, Functor f) {
   auto grainsize = get_policy<satyr::grainsize>(policy);
   auto range = k_blocked_range(extents, grainsize.value);
-  tbb::parallel_for(detail::tbb_k_blocked_range(range), [=](const auto& range) {
+  tbb::parallel_for(detail::tbb_blocked_range(range), [=](const auto& range) {
     for_each_index(policy | serial_v, range.range, f);
+  });
+}
+
+//------------------------------------------------------------------------------
+// for_each_index_triangular
+//------------------------------------------------------------------------------
+template <uplo_t Uplo, Policy Policy, IndexFunctor<2> Functor>
+  requires has_policy_v<grainsize, Policy>
+void for_each_index_triangular(Policy policy, index_t n, Functor f) {
+  auto grainsize = get_policy<satyr::grainsize>(policy);
+  auto range = triangular_blocked_range<Uplo>{n, grainsize};
+  tbb::parallel_for(detail::tbb_blocked_range(range), [=](const auto& range) {
+    for_each_index_triangular<Uplo>(policy | serial_v, range.range, f);
   });
 }
 
 //------------------------------------------------------------------------------
 // for_with_exit
 //------------------------------------------------------------------------------
-namespace detail {
-template <Policy Policy, IndexPredicate<1> F>
-  requires has_policy_v<grainsize, Policy>
-void for_with_exit_impl(tbb::task_group_context& group, Policy policy,
-                          index_t first, index_t last, F f) {
-  auto grainsize = get_policy<satyr::grainsize>(policy);
-  tbb::parallel_for(
-      tbb::blocked_range<index_t>{first, last,
-                                  static_cast<size_t>(grainsize.value)},
-      [=, &group](const tbb::blocked_range<index_t>& range) {
-        if (!for_with_exit(policy | serial_v, range.begin(), range.end(), f))
-          group.cancel_group_execution();
-      },
-      tbb::auto_partitioner{}, group);
-}
-} // namespace detail
-
 template <Policy Policy, IndexPredicate<1> F>
   requires has_policy_v<grainsize, Policy>
 bool for_with_exit(Policy policy, index_t first, index_t last, F f) {
   auto grainsize = get_policy<satyr::grainsize>(policy);
   auto range = tbb::blocked_range<index_t>{
       first, last, static_cast<size_t>(grainsize.value)};
-  if (!range.is_divisible()) {
+  if (!range.is_divisible())
     return for_with_exit(policy | serial_v, first, last, f);
-  }
   tbb::task_group_context group;
   tbb::parallel_for(
       range,
@@ -112,9 +107,8 @@ bool for_each_index_with_exit(Policy policy, std::array<index_t, K> extents,
     Predicate f) {
   auto grainsize = get_policy<satyr::grainsize>(policy);
   auto range = k_blocked_range(extents, static_cast<size_t>(grainsize.value));
-  if (!range.is_divisible()) {
+  if (!range.is_divisible())
     return for_each_index_with_exit(policy | serial_v, extents, f);
-  }
   tbb::task_group_context group;
   tbb::parallel_for(
       range,
@@ -129,46 +123,21 @@ bool for_each_index_with_exit(Policy policy, std::array<index_t, K> extents,
 //------------------------------------------------------------------------------
 // for_each_index_triangular_with_exit
 //------------------------------------------------------------------------------
-namespace detail {
-template <uplo_t Uplo, class Policy, class Predicate>
-    requires Uplo == uplo_t::lower 
-void for_each_index_triangular_with_exit_impl(tbb::task_group_context& group,
-                                                Policy policy, index_t j,
-                                                index_t n, Predicate f) {
-  for_with_exit_impl(group, policy, j, n, [=](index_t i) { return f(i, j); });
-}
-
-template <uplo_t Uplo, class Policy, class Predicate>
-    requires Uplo == uplo_t::upper 
-void for_each_index_triangular_with_exit_impl(tbb::task_group_context& group,
-                                                Policy policy, index_t j,
-                                                index_t n, Predicate f) {
-  for_with_exit_impl(group, policy, 0, j + 1,
-                       [=](index_t i) { return f(i, j); });
-}
-}  // namespace detail
-
-template <uplo_t Uplo, Policy Policy, IndexPredicate<2> Functor>
+template <Policy Policy, uplo_t Uplo, IndexPredicate<2> Predicate>
   requires has_policy_v<grainsize, Policy>
 bool for_each_index_triangular_with_exit(Policy policy, index_t n,
-                                           Functor f) {
-  auto grainsize_outer = subdivide(get_policy<grainsize>(policy), n + 1);
-  auto n_div_2 = n / 2;
-  auto p = n / 2 + (n % 2);
+                                         Predicate f) {
+  auto grainsize = get_policy<satyr::grainsize>(policy);
+  auto range = triangular_blocked_range<Uplo>{n, grainsize};
+  if (!range.is_divisible())
+    return for_each_index_triangular_with_exit<Uplo>(policy | serial_v, n, f);
   tbb::task_group_context group;
-  detail::for_with_exit_impl(
-      group, grainsize_outer, 0, p, [=, &group](index_t j1) {
-        auto j2 = n - j1 - 1;
-        if (j1 != j2) {
-          detail::for_each_index_triangular_with_exit_impl<Uplo>(
-              group, policy, j1, n, f);
-          detail::for_each_index_triangular_with_exit_impl<Uplo>(
-              group, policy, j2, n, f);
-        } else {
-          detail::for_each_index_triangular_with_exit_impl<Uplo>(
-              group, policy, j1, n, f);
-        }
-      });
+  tbb::parallel_for(detail::tbb_blocked_range(range),
+                    [=, &group](const auto& range) {
+                      if (!for_each_index_triangular_with_exit<Uplo>(
+                              policy | serial_v, range.range, f))
+                        group.cancel_group_execution();
+                    });
   return !group.is_group_execution_cancelled();
 }
 } // namespace satyr
