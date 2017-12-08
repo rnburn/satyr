d7e33277cfcbde0a770db5347ada74e9fd248abd
Ryan Burn <ryan.burn@gmail.com>
2017-12-08 14:04:10 -0800

    Add script to run bazel tests.

---
+++ ci/do_ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+export SATYR_SRC_DIR=/src
+export SATYR_CI_DIR=${SATYR_SRC_DIR}/ci
+
+cd "${SATYR_CI_DIR}"
+
+export BAZEL_OPTIONS="--package_path=%workspace%:${SATYR_SRC_DIR}"
+
+case "$1" in
+  bazel.debug)
+    echo "Testing..."
+    bazel test ${BAZEL_OPTIONS} -c dbg //...
+    ;;
+  *)
+    echo $"Usage: $0 {bazel.debug}"
+    exit 1
+esac
