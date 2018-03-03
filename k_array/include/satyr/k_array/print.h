a1eab20c46213349b60d76947ca767fa2367f72c
Ryan Burn <ryan.burn@gmail.com>
2018-03-03 17:05:19 -0500

    Add function to print arrays in json format.

--- k_array/include/satyr/k_array/print.h
+++ k_array/include/satyr/k_array/print.h
@@ -10,24 +10,31 @@ namespace satyr {
 // print_impl
 //------------------------------------------------------------------------------
 namespace detail {
+struct ArrayPrintFormat {
+  const char* open_brace = "{";
+  const char* close_brace = "}";
+  const char* separator = ", ";
+};
+
 template <size_t K, class Accessor>
-void print_impl(std::ostream& out, const std::array<index_t, K>& extents,
-  const Accessor& accessor) {
+void print_impl(const ArrayPrintFormat& format, std::ostream& out,
+                const std::array<index_t, K>& extents,
+                const Accessor& accessor) {
   if constexpr (K == 0) {
     out << accessor();
   } else {
-    out << '{';
+    out << format.open_brace;
     for (index_t index=0; index<extents[0]; ++index) {
-      print_impl(out,
+      print_impl(format, out,
                  *reinterpret_cast<const std::array<index_t, K - 1>*>(
                      extents.data() + 1),
                  [&](auto... indexes_rest) {
                    return accessor(index, indexes_rest...);
                  });
       if (index != extents[0]-1)
-        out << ", ";
+        out << format.separator;
     }
-    out << '}';
+    out << format.close_brace;
   }
 }
 } // namespace detail
@@ -40,7 +47,8 @@ template <class T, size_t K>
     out << t;
   }
 std::ostream& operator<<(std::ostream& out, const k_array_view<T, K>& array) {
-  detail::print_impl(out, array.shape().extents(), array);
+  detail::print_impl(detail::ArrayPrintFormat{}, out, array.shape().extents(),
+                     array);
   return out;
 }
 
@@ -49,7 +57,29 @@ template <class T, size_t K>
     out << t;
   }
 std::ostream& operator<<(std::ostream& out, const k_array_subview<T, K>& array) {
-  detail::print_impl(out, array.shape().extents(), array);
+  detail::print_impl(detail::ArrayPrintFormat{}, out, array.shape().extents(),
+                     array);
   return out;
+}
+
+//------------------------------------------------------------------------------
+// print_json
+//------------------------------------------------------------------------------
+template <class T, size_t K>
+  requires requires (std::ostream& out, T t) {
+    out << t;
+  }
+void print_json(std::ostream& out, const k_array_view<T, K>& array) {
+  detail::print_impl(detail::ArrayPrintFormat{"[", "]", ","}, out,
+                     array.shape().extents(), array);
+}
+
+template <class T, size_t K>
+  requires requires (std::ostream& out, T t) {
+    out << t;
+  }
+void print_json(std::ostream& out, const k_array_subview<T, K>& array) {
+  detail::print_impl(detail::ArrayPrintFormat{"[", "]", ","}, out,
+                     array.shape().extents(), array);
 }
 } // namespace satyr
--- n_array/include/satyr/n_array/print.h
+++ n_array/include/satyr/n_array/print.h
@@ -10,14 +10,32 @@ namespace satyr {
 template <Scalar T, size_t K, Structure Structure>
 std::ostream& operator<<(std::ostream& out,
                          const n_array_view<T, K, Structure>& array) {
-  detail::print_impl(out, array.shape().extents(), array);
+  detail::print_impl(detail::ArrayPrintFormat{}, out, array.shape().extents(),
+                     array);
   return out;
 }
 
 template <Scalar T, size_t K, Structure Structure>
 std::ostream& operator<<(std::ostream& out,
                          const n_array_subview<T, K, Structure>& array) {
-  detail::print_impl(out, array.shape().extents(), array);
+  detail::print_impl(detail::ArrayPrintFormat{}, out, array.shape().extents(),
+                     array);
   return out;
+}
+
+//------------------------------------------------------------------------------
+// print_json
+//------------------------------------------------------------------------------
+template <Scalar T, size_t K, Structure Structure>
+void print_json(std::ostream& out, const n_array_view<T, K, Structure>& array) {
+  detail::print_impl(detail::ArrayPrintFormat{"[", "]", ","}, out,
+                     array.shape().extents(), array);
+}
+
+template <Scalar T, size_t K, Structure Structure>
+void print_json(std::ostream& out,
+                const n_array_subview<T, K, Structure>& array) {
+  detail::print_impl(detail::ArrayPrintFormat{"[", "]", ","}, out,
+                     array.shape().extents(), array);
 }
 } // namespace satyr
